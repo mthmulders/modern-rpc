97a6ceadaa2325e47e58009943881023892d3455
Maarten Mulders <m.th.mulders@xs4all.nl>
2021-04-10 19:59:10 +0200

    Log incoming calls

--- server/rmi/src/main/java/it/mulders/modernrpc/rmi/server/FibonacciRmiServer.java
+++ server/rmi/src/main/java/it/mulders/modernrpc/rmi/server/FibonacciRmiServer.java
@@ -3,8 +3,12 @@ package it.mulders.modernrpc.rmi.server;
 import it.mulders.modernrpc.rmi.shared.Fibonacci;
 import it.mulders.modernrpc.rmi.shared.FibonacciOutput;
 import it.mulders.modernrpc.server.core.FibonacciCalculator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FibonacciRmiServer implements Fibonacci {
+    private static final Logger logger = LoggerFactory.getLogger(FibonacciRmiServer.class);
+
     private final FibonacciCalculator calculator;
 
     public FibonacciRmiServer(final FibonacciCalculator calculator) {
@@ -13,6 +17,7 @@ public class FibonacciRmiServer implements Fibonacci {
 
     @Override
     public FibonacciOutput fibonacci(int input) {
+        logger.debug("Calculating Fibonacci number with input {}", input);
         var result = calculator.fibonacci(input);
         return new FibonacciOutput(result.result(), result.duration());
     }
