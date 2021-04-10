d4c680b7857fdd50de9a338bffc13aabf0c41564
Maarten Mulders <m.th.mulders@xs4all.nl>
2021-04-10 14:11:10 +0200

    Add assembled server

---
+++ server/assembly/src/main/java/it/mulders/modernrpc/FibonacciServer.java
@@ -0,0 +1,58 @@
+package it.mulders.modernrpc;
+
+import it.mulders.modernrpc.server.core.FibonacciCalculator;
+import it.mulders.modernrpc.server.core.FibonacciServerStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.ServiceLoader;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.stream.Collectors.toList;
+
+public class FibonacciServer {
+    private static final Logger logger = LoggerFactory.getLogger(FibonacciServer.class);
+
+    public static void main(final String... args) throws InterruptedException {
+        logger.info("Starting Fibonacci servers");
+
+        var calculator = new FibonacciCalculator();
+
+        var threadFactory = new ThreadFactory() {
+            @Override
+            public Thread newThread(final Runnable r) {
+                var thread = new Thread(r);
+                return thread;
+            }
+        };
+
+        var executorService = Executors.newFixedThreadPool(8, threadFactory);
+
+        var strategies = findServerStrategies();
+
+        strategies.stream()
+                .peek(strategy -> logger.info("Found {} server", strategy.getName()))
+                .map(strategy -> strategy.start(executorService, calculator))
+                .forEach(CompletableFuture::join);
+
+        logger.info("Press [enter] to stop the servers");
+
+        System.console().readLine();
+
+        strategies.forEach(FibonacciServerStrategy::stop);
+        logger.info("All servers stopped");
+
+        executorService.shutdown();
+        logger.info("Executor Service shut down");
+        executorService.awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    static List<FibonacciServerStrategy> findServerStrategies() {
+        var loader = ServiceLoader.load(FibonacciServerStrategy.class);
+        return loader.stream().map(ServiceLoader.Provider::get).collect(toList());
+    }
+}
---
+++ server/core/src/main/java/it/mulders/modernrpc/server/core/ServerException.java
@@ -0,0 +1,7 @@
+package it.mulders.modernrpc.server.core;
+
+public class ServerException extends Exception {
+    public ServerException(final Throwable cause) {
+        super(cause);
+    }
+}
